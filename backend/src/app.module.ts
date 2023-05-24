8a62a91ab02f4276230eb22cdd7fc6185f1b8018
ishakzail <ishak.zail.iz@gmail.com>
2023-05-24 21:02:22 +0100

    new: API (signin/signup)

---
+++ backend/src/app.module.ts
@@ -0,0 +1,21 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+
+import { AuthModule } from './auth/auth.module';
+import { PrismaModule } from './prisma/prisma.module';
+import { UserController } from './user/user.controller';
+import { UserModule } from './user/user.module';
+import { UserService } from './user/user.service';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true
+    }),
+    AuthModule, 
+    PrismaModule, UserModule,
+  ],
+  controllers: [UserController],
+  providers : [UserService]
+})
+export class AppModule {}
