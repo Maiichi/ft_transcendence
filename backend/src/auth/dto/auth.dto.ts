d4e99eccdc5c6a64bf89246c8c71631d02c19957
ishakzail <ishak.zail.iz@gmail.com>
2023-05-29 18:55:34 +0100

    update dto for data persistance

--- backend/src/auth/dto/auth.dto.ts
+++ backend/src/auth/dto/auth.dto.ts
@@ -1,15 +1,29 @@
 import { 
     IsEmail, 
     IsNotEmpty, 
+    IsNumber, 
     IsString 
 } from "class-validator";
 
 export class AuthDto {
+
     @IsEmail()
     @IsNotEmpty()
     email : string;
 
     @IsString()
-    @IsNotEmpty()
-    password: string;
+    first_name: string;
+
+    @IsString()
+    last_name: string;
+
+    @IsString()
+    username: string;
+
+    @IsNumber()
+    intra_id: number;
+    
+    @IsString()
+    avatar_url: string;
+
 }
