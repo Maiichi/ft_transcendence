406f020cb69055d9bad2a162f5c2d557971fbba6
ibouroum <ibouroummana@gmail.com>
2023-07-10 22:17:49 +0100

    feat(FRONT): adding layout to router components.

--- Client/src/packages/feat-Home/Home.tsx
+++ Client/src/packages/feat-Home/Home.tsx
@@ -1,13 +1,22 @@
 import { useEffect } from "react";
+import { redirect, useNavigate } from "react-router-dom";
 import { useAppDispatch, useAppSelector } from "../../core";
 
 export const Home = () => {
+  const navigate = useNavigate();
+  // navigate("/test");
   // const state: CounterState = useAppSelector((state) => state.counter);
   // const todos: TodosState = useAppSelector((state) => state.todos);
   // const dispatch = useAppDispatch();
   // useEffect(() => {
   //   dispatch(fetchTodos(4));
   // }, [dispatch]);
-
-  return <>Hello world</>;
+  const han = () => {
+    navigate("/test");
+  };
+  return (
+    <>
+      <button onClick={() => han()}>sss</button>
+    </>
+  );
 };
