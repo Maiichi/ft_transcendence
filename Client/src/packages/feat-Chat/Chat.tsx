6c84089bf58df0f571095ac7aaa9bff7d0d4caf1
ishakzail <ishak.zail.iz@gmail.com>
2023-08-14 12:23:59 +0100

    feat(FRONT): updating chat UI

--- Client/src/packages/feat-Chat/Chat.tsx
+++ Client/src/packages/feat-Chat/Chat.tsx
@@ -29,50 +29,71 @@ export const Chat = () => {
 
   return (
     <Root>
-      <ChatNavbar>
-        <SearchComponent />
-        <Tabs>
-          {[1, 2, 3].map((item) => (
-            <Tab className="Tab-holder">
-              <Avatar className="Avatar"
-                sx={{ width: "40px", height: "40px" }}
-                alt="Remy Sharp"
-                src="/static/images/avatar/1.jpg"
-              />
-              <Title className="room-user-status-holder">
-                <H4 className="room-user-name">Ishak Zail</H4>
-                <H5>Online</H5>
-              </Title>
-            </Tab>
-          ))}
-        </Tabs>
-      </ChatNavbar>
-      <MessageContent>
-        <Header>
-          <Title className="room-user-status-holder">
-            <Avatar
-              className="Avatar"
-              sx={{ width: "40px", height: "40px" }}
-              alt="Remy Sharp"
-              src="/static/images/avatar/1.jpg"
-              style={{ margin: "5px" }}
-            />
-            <H4 className="room-user-name">Ishak Zail</H4>
-          </Title>
-          <Button >option</Button>
-        </Header>
-        <Separator />
-        <Messages>messages</Messages>
-        <Separator />
-        <WriteMessage>
-          <input
-            type="text"
-            placeholder="Type a message..."
-            value={msg}
-          />
-          <Button>Send</Button>
-        </WriteMessage>
-      </MessageContent>
+        <Discussions>
+          <SearchComponent/>
+
+          <Discussion>
+            <Avatar style={{
+              marginLeft : "20px",
+              display: "block",
+              width: "45px",
+              height : "45px",
+              background : "#E6E7ED",
+              backgroundPosition : "center",
+              backgroundSize : "cover",
+              backgroundRepeat : "no-repeat",
+              MozBorderRadius : "50px",
+              WebkitBorderRadius : "50px"
+            }}>
+              <Online/>
+            </Avatar>
+            <ContactDesc>
+              <Name> Ishak Zail </Name>
+              <LastMessage> hello !  </LastMessage>
+            </ContactDesc>
+          </Discussion>
+        </Discussions>
+        <ChatSection>
+            <ChatHeader>
+                <ChatName>Ishak ZAIL</ChatName>
+            </ChatHeader>
+            <ChatMessages>
+                <Messages>
+                    <Avatar style ={{
+                        marginLeft : "20px",
+                        display: "block",
+                        width: "45px",
+                        height : "45px",
+                        background : "#E6E7ED",
+                        backgroundPosition : "center",
+                        backgroundSize : "cover",
+                        backgroundRepeat : "no-repeat",
+                        MozBorderRadius : "50px",
+                        WebkitBorderRadius : "50px"
+                    }} />
+                    <MessageTextSender>
+                        wa feen
+                    </MessageTextSender>
+                </Messages>
+                <Messages>
+                    <Avatar style ={{
+                        marginLeft : "20px",
+                        display: "block",
+                        width: "45px",
+                        height : "45px",
+                        background : "#E6E7ED",
+                        backgroundPosition : "center",
+                        backgroundSize : "cover",
+                        backgroundRepeat : "no-repeat",
+                        MozBorderRadius : "50px",
+                        WebkitBorderRadius : "50px"
+                    }} />
+                    <MessageTextReceiver>
+                        wa feen
+                    </MessageTextReceiver>
+                </Messages>
+            </ChatMessages>
+        </ChatSection>        
     </Root>
   );
 };
@@ -81,67 +102,102 @@ const Root = styled.div`
   height: -webkit-fill-available;
   display: flex;
   background-color: rgb(255, 255, 255);
-  border: 1px solid rgba(0, 0, 0, 0.125);
+  // border: 1px solid rgba(0, 0, 0, 0.125);
   border-radius: 0.25rem;
 `;
-const ChatNavbar = styled.div`
-  padding: 15px;
-  border-right: 1px solid #dee2e6 !important;
-  width: 30%;
-`;
 
-const MessageContent = styled.div`
-  width: 70%;
-`;
-
-const H4 = styled.h4`
-  color: red;
-  margin-top: 0px;
-  margin-bottom: 0px;
-`;
-const H5 = styled.h4`
-  color: blue;
-  margin-top: 0px;
-  margin-bottom: 0px;
-`;
-
-const Tab = styled.div`
-  display: flex;
-  margin-top: 5px;
-  margin-bottom: 0px;
-  padding: 5px 0px 5px 5px;
-`;
-const Tabs = styled.div`
- 
-  margin-top: 15px;
-`;
-
-const Title = styled.div`
-  color: black;
-  margin: 2px 0px 0px 10px;
-  display: flex;
-`;
-
-const Header = styled.div`
-  background-color: grey;
-  display: flex;
-  justify-content: space-between; /* Add this line */
-  align-items: center; /* Center vertically */
+const Discussions = styled.section`
+  width: 35%;
+  height: 700px;
+  box-shadow: 0px 8px 10px rgba(0,0,0,0.20);
+  overflow: hidden;
+  background-color: #87a3ec;
+  display: inline-block;
 `
 
+const Discussion = styled.div`
+  width: 100%;
+  height: 90px;
+  background-color: #FAFAFA;
+  border-bottom: solid 1px #E0E0E0;
+  display:flex;
+  align-items: center;
+  cursor: pointer;
+`
+
+const ContactDesc = styled.div`
+height: 43px;
+width: 50%;
+white-space: nowrap;
+overflow: hidden;
+text-overflow: ellipsis;
+`
+
+const MessageActive = styled.div`
+  width: 98.5%;
+  height: 90px;
+  background-color: #FFF;
+  border-bottom: solid 1px #E0E0E0;
+`
+
+const Online = styled.div`
+  position: relative;
+  top: 30px;
+  left: 35px;
+  width: 13px;
+  height: 13px;
+  background-color: #8BC34A;
+  border-radius: 13px;
+  border: 3px solid #FAFAFA;
+`
+
+const Name = styled.p`
+  margin: 0 0 0 20px;
+  font-family:'Montserrat', sans-serif;
+  font-size: 11pt;
+  color:#515151;
+`
+
+const LastMessage = styled.p`
+  margin: 6px 0 0 20px;
+  font-family:'Montserrat', sans-serif;
+  font-size: 9pt;
+  color:#515151;
+`
+
+const ChatSection = styled.section`
+  width: calc(65% - 85px);
+`
+const ChatHeader = styled.div`
+  background-color: #FFF;
+  height: 90px;
+  box-shadow: 0px 3px 2px rgba(0,0,0,0.100);
+  display:flex;
+  align-items: center;
+`
+const ChatName = styled.p`
+  margin: 0 0 0 20px;
+  text-transform: uppercase;
+  font-family:'Montserrat', sans-serif;
+  font-size: 13pt;
+  color:#515151;
+`
+
+const ChatMessages = styled.div`
+    padding: 25px 35px;
+`
 const Messages = styled.div`
-  background-color: blue;
+    display:flex;
+    align-items: center;
+    margin-bottom: 8px;
 `
-const WriteMessage = styled.div`
-  background-color: green;
+const MessageTextSender = styled.p`
+    margin: 0 35px;
+    background-color: #f6f6f6;
+    padding: 15px;
+    border-radius: 12px;
 `
 
-const Separator = styled.div`
-  height: 1px;
-  background-color: black; /* Color of the separator */
-  margin: 10px 0; /* Adjust the margin as needed */
-`;
-
-const Button = styled.button`
-  margin-right: 5px;
+const MessageTextReceiver = styled.p`
+    margin-left: 45px;
 `
