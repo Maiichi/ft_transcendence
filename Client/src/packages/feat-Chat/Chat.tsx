53db6d050ca4e7056219843f7325c808bc4fa1e2
ishakzail <ishak.zail.iz@gmail.com>
2023-08-19 18:44:22 +0100

    feat(FRONT): add Dms and Rooms buttons

--- Client/src/packages/feat-Chat/Chat.tsx
+++ Client/src/packages/feat-Chat/Chat.tsx
@@ -12,6 +12,9 @@ import { Avatar } from "@mui/material";
 import{ Message }from "./message/Message"
 import './chat.css';
 import { ChatUsers } from "./chatUsers/ChatUsers";
+import { Search } from "@mui/icons-material";
+import MapsUgcIcon from '@mui/icons-material/MapsUgc';
+import DriveFileRenameOutlineTwoToneIcon from '@mui/icons-material/DriveFileRenameOutlineTwoTone';
 
 export const Chat = () => {
   const state: ChatIshakState = useAppSelector((state) => state.chat);
@@ -20,6 +23,7 @@ export const Chat = () => {
   const chatState = useAppSelector((state) => state.chat);
   const [name, setName] = useState("");
   const [msg, setMsg] = useState("");
+  const [selectedTab, setSelectedTab]  = useState('Dms');
 
   const handleClick = () => {
     dispatch(sendMessage({ name, msg }));
@@ -30,44 +34,70 @@ export const Chat = () => {
     dispatch(clearState());
   };
 
+  const discussions = [
+    {
+      id: 1,
+      type: 'Dms',
+      name: 'Dave Corlew',
+      message: "Let's meet for a coffee or something today ?",
+      timer: '3 min'
+    },
+    {
+      id: 2,
+      type: 'Rms',
+      name: 'Room 1',
+      message: "Let's meet for a coffee or something today ?",
+      timer: '3 min'
+    }
+  ];
+  const filteredDiscussions = discussions.filter(
+    (discussion) => discussion.type === selectedTab
+  );
   return (
     <Root>
         <div className="discussions">
-          <div className="discussion search">
-            <div className="searchbar">
-              <i className="fa fa-search" aria-hidden="true"></i>
-              <input type="text" placeholder="Search..."></input>
+          <div className="discussion-search">
+            <div className="chatTitleHolder">
+              <p className="messages-text">
+                Messages
+              </p>
+              
+            </div>
+            <div className="searchIconHolder">
+                <Search></Search>
+            </div>
+            {/* need to add a model when clicking on this ICON */}
+            <div className="newMessageHolder">
+              <DriveFileRenameOutlineTwoToneIcon ></DriveFileRenameOutlineTwoToneIcon>
             </div>
           </div>
           <div className="discussion-room-user">
-            <button className="rooms">
-              RMs
+            <button 
+              className={`rooms ${selectedTab === 'Dms' ? 'active' : ''}`}
+              onClick={() => setSelectedTab('Dms')}
+            >
+              Dms
             </button>
-            <button className="dm">
-                DMs
+            <button 
+             className={`dm ${selectedTab === 'Rms' ? 'active' : ''}`}
+             onClick={() => setSelectedTab('Rms')}
+            >
+                Rms
             </button>
           </div>
-          <div className="discussion message-active">
-            <div className="photo">
-              <div className="online"></div>
+          {filteredDiscussions.map((discussion) => (
+            <div key={discussion.id} className="discussion">
+              <div className="photo">
+                <div className="online"></div>
+              </div>
+              <div className="desc-contact">
+                <p className="name">{discussion.name}</p>
+                <p className="message">{discussion.message}</p>
+              </div>
+              <div className="timer">{discussion.timer}</div>
             </div>
-            <div className="desc-contact">
-              <p className="name">Megan Leib</p>
-              <p className="message">9 pm at the bar if possible 😳</p>
-            </div>
-            <div className="timer">12 sec</div>
-          </div>
-
-          <div className="discussion">
-            <div className="photo" >
-              <div className="online"></div>
-            </div>
-            <div className="desc-contact">
-              <p className="name">Dave Corlew</p>
-              <p className="message">Let's meet for a coffee or something today ?</p>
-            </div>
-            <div className="timer">3 min</div>
-          </div>
+          ))}
+         
         </div>
         <div className="chatBox">
           <div className="chatBoxHeader">
@@ -100,7 +130,7 @@ export const Chat = () => {
             <button className="chatSubmitButtom">Send</button>
           </div>
         </div>
-        <ChatUsers/>
+        <ChatUsers type={selectedTab}/>
     </Root>
   );
 };
@@ -113,216 +143,6 @@ const Root = styled.div`
   // padding: 0;
   // margin: 5px;
   height: 900px;
+  border-radius: 20px:
 
 `;
-
-// const Discussions = styled.section`
-//   width: 30%;
-//   height: 100%;
-//   box-shadow: 0px 8px 10px rgba(0,0,0,0.20);
-//   overflow: hidden;
-//   background-color: #87a3ec;
-//   display: inline-block;
-// `
-
-// const Discussion = styled.div`
-//   width: 100%;
-//   height: 90px;
-//   background-color: #FAFAFA;
-//   border-bottom: solid 1px #E0E0E0;
-//   display:flex
-//   align-items: center;
-//   cursor: pointer;
-// `
-
-// const ContactDesc = styled.div`
-//   height: 43px;
-//   width: 50%;
-//   white-space: nowrap;
-//   overflow: hidden;
-//   text-overflow: ellipsis;
-// `
-
-// const MessageActive = styled.div`
-//   width: 98.5%;
-//   height: 90px;
-//   background-color: #FFF;
-//   border-bottom: solid 1px #E0E0E0;
-// `
-
-// const Online = styled.div`
-//   position: relative;
-//   top: 30px;
-//   left: 35px;
-//   width: 13px;
-//   height: 13px;
-//   background-color: #8BC34A;
-//   border-radius: 13px;
-//   border: 3px solid #FAFAFA;
-// `
-
-// const Name = styled.p`
-//   margin: 0 0 0 20px;
-//   font-family:'Montserrat', sans-serif;
-//   font-size: 11pt;
-//   color:#515151;
-// `
-
-// const LastMessage = styled.p`
-//   margin: 6px 0 0 20px;
-//   font-family:'Montserrat', sans-serif;
-//   font-size: 9pt;
-//   color:#515151;
-// `
-
-// const ChatSection = styled.section`
-//   width: calc(65% - 85px);
-//   background-color: red;
-// `
-// const ChatHeader = styled.div`
-//   background-color: #FFF;
-//   height: 90px;
-//   box-shadow: 0px 3px 2px rgba(0,0,0,0.100);
-//   display:flex;
-//   align-items: center;
-// `
-// const ChatName = styled.p`
-//   margin: 0 0 0 20px;
-//   text-transform: uppercase;
-//   font-family:'Montserrat', sans-serif;
-//   font-size: 13pt;
-//   color:#515151;
-// `
-
-// const ChatMessages = styled.div`
-//     padding: 25px 35px;
-// `
-
-// const UserMessage = styled.div`
-//   display:flex;
-//   align-items: center;
-//   margin-bottom: 8px; 
-// `
-// const MessageText = styled.p`
-//   margin: 0 35px;
-//   background-color: #f6f6f6;
-//   padding: 15px;
-//   border-radius: 12px;  
-// `
-
-// const SenderMessage = styled.div`
-// display:flex;
-// align-items: center;
-// margin-bottom: 8px; 
-// `
-
-// const MessageTextOnly = styled.div`
-//   margin-left: 45px;
-// `
-// const Response = styled.div`
-//   float: right;
-//   margin-right: 0px !important;
-//   margin-left:auto;
-// `
-
-// const ResponseText = styled.p`
-//   background-color: #e3effd !important;
-//   margin: 0px;
-//   padding: 15px;
-//   border-radius: 12px;  
-// `
-
-// // const FooterChat = styled.div`
-// // width: calc(65% - 66px);
-// // height: 80px;
-// // display:flex;
-// // align-items: center;
-// // position:absolute;
-// // bottom: 0;
-// // background-color: transparent;
-// // border-top: 2px solid #EEE;
-// // `
-
-// // const FooterChatIcon = styled.i`
-// // margin-left: 30px;
-// // color:#C0C0C0;
-// // font-size: 14pt;
-// // cursor: pointer;
-// // `
-
-// // const FooterChatSend = styled.i`
-// //   color:#fff;
-// //   background-color: #4f6ebd;
-// //   position: absolute;
-// //   right: 50px;
-// //   padding: 12px 12px 12px 12px;
-// //   border-radius: 50px;
-// //   font-size: 14pt;
-// //   cursor: pointer;
-// // `
-
-// // const FooterChatMessage = styled.input`
-// //   border:none !important;
-// //   width:60%;
-// //   height: 50px;
-// //   margin-left: 20px;
-// //   padding: 10px;
-// //   cursor: pointer;
-// // `
-
-// const FooterChat = styled.div`
-//   width: calc(65% - 80px);
-//   height: 80px;
-//   display: flex;
-//   align-items: center;
-//   position: absolute;
-//   bottom: 0;
-//   background-color: transparent;
-//   border-top: 2px solid #eee;
-//   margin: 20px;
-// `;
-
-// const Icon = styled.i`
-//   margin-left: 30px;
-//   color: #c0c0c0;
-//   font-size: 14pt;
-// `;
-
-// const SendIcon = styled.i`
-//   color: #fff;
-//   background-color: #4f6ebd;
-//   position: absolute;
-//   right: 50px;
-//   padding: 12px;
-//   border-radius: 50px;
-//   font-size: 14pt;
-// `;
-
-// const WriteMessageInput = styled.input`
-//   border: none !important;
-//   width: 60%;
-//   height: 50px;
-//   margin-left: 20px;
-//   padding: 10px;
-//   ::-webkit-input-placeholder {
-//     color: #c0c0c0;
-//     font-size: 13pt;
-//   }
-//   :-moz-placeholder {
-//     color: #c0c0c0;
-//     font-size: 13pt;
-//   }
-//   ::-moz-placeholder {
-//     color: #c0c0c0;
-//     font-size: 13pt;
-//     margin-left: 5px;
-//   }
-//   :-ms-input-placeholder {
-//     color: #c0c0c0;
-//     font-size: 13pt;
-//   }
-// `;
-
-// const ClickableIcon = styled(Icon)`
-//   cursor: pointer;
-// `;
--- Client/src/packages/feat-Chat/chatUsers/ChatUsers.tsx
+++ Client/src/packages/feat-Chat/chatUsers/ChatUsers.tsx
@@ -1,42 +1,89 @@
 import React from 'react'
 import "./chatUsers.css"
+import { ButtonAvatar, CardAvatar, H5 } from '../../feat-Account/components'
+import { Avatar, Divider, Icon, Popper } from '@mui/material'
+import { Title } from '@mui/icons-material'
+import MoreHorizRoundedIcon from '@mui/icons-material/MoreHorizRounded';
+import { PopperComponent } from '../../../core'
 
-export const ChatUsers = () => {
+interface Props {
+    type: string
+}
+
+const isFriend = false;
+const usersInRoom = [
+    {
+        name: 'Ishak Zail',
+        isOnline: true,
+        isFriend: false,
+    },
+    {
+        name: 'Ismail Bouroummana',
+        isOnline: false,
+        isFriend: true,
+    }
+]
+
+export const ChatUsers = ({type} : Props) => {
+    const ButtonData = isFriend ? 'Invite to game' : 'Send Request';
   return (
     <div className="onlineUsersBox">
-        <div className="onlineUsersHeader">
-        <h3 className="onlineUsersTop">
-            ONLINE USERS
-        </h3>
-        </div>
-        <div className="onlineUsers">
-            <div className="onlineUserImgContainer">
-                <img 
-                    className='onlineUserImg'
-                    src="https://media.istockphoto.com/id/1294780139/photo/close-up-portrait-of-smiling-man-with-eyeglasses-in-blue-shirt-3d-illustration-of-cartoon.jpg?s=1024x1024&w=is&k=20&c=6ENyB-NdL-HZJOtV6Jp8SD8TRx9w_KLswfIH9s6uGUs=" alt="" />
-                <div className="onlineUsersBadge">
-            </div>
-            
+        {
+            type === 'Dms' ? (
+                <>
+                <div className="onlineUsersHeader">
+                    <h3 className="onlineUsersTop">
+                        Online Users
+                    </h3>
+                </div>
+                <div className="cardHolder">
+                    <CardAvatar>
+                        <Avatar
+                            sx={{ width: "80px", height: "80px" }}
+                            alt="Remy Sharp"
+                            src="/static/images/avatar/1.jpg"
+                        />
+                        <Title style={{ fontSize: "1rem" }}>Bouroummana Ismail</Title>
+                        <H5>ibouroum</H5>
+                        <Divider />
+                        <ButtonAvatar>{ButtonData}</ButtonAvatar>
+                        <ButtonAvatar>View Profile</ButtonAvatar>
+                    </CardAvatar>
+                </div>
+                <div className="divider">
 
-            </div>
-            <p className="onlineUsersName">
-                Ishak Zail
-            </p>
-        </div>
-        <div className="onlineUsers">
-            <div className="onlineUserImgContainer">
-                <img 
-                    className='onlineUserImg'
-                    src="https://media.istockphoto.com/id/1294780139/photo/close-up-portrait-of-smiling-man-with-eyeglasses-in-blue-shirt-3d-illustration-of-cartoon.jpg?s=1024x1024&w=is&k=20&c=6ENyB-NdL-HZJOtV6Jp8SD8TRx9w_KLswfIH9s6uGUs=" alt="" />
-                <div className="onlineUsersBadge">
-            </div>
-            
-
-            </div>
-            <p className="onlineUsersName">
-                Ishak Zail
-            </p>
-        </div>
+                </div>
+                <div className="onlineUsers">
+                    <div className="onlineUserImgContainer">
+                        <img 
+                            className='onlineUserImg'
+                            src="https://media.istockphoto.com/id/1294780139/photo/close-up-portrait-of-smiling-man-with-eyeglasses-in-blue-shirt-3d-illustration-of-cartoon.jpg?s=1024x1024&w=is&k=20&c=6ENyB-NdL-HZJOtV6Jp8SD8TRx9w_KLswfIH9s6uGUs=" alt="" />
+                        <div className="onlineUsersBadge">
+                        </div>
+                    </div>
+                    <p className="onlineUsersName">
+                        Ishak Zail
+                    </p>
+                    <div className="i0conHolder">
+                        {/* < /> */}
+                    </div>
+                </div>
+                </>
+            )
+            : (
+                <>
+                <div className="onlineUsersHeader">
+                    <h3 className="onlineUsersTop">
+                        Room Users
+                    </h3>
+                    
+                </div>
+                <div className="onlineUsers">
+                        Room logic
+                </div>
+                </>
+            )
+        }
     </div>
   )
 }
