ce7d476d36a743a3750e0d83811c1cd56dc3fee1
ibouroum <ibouroummana@gmail.com>
2023-09-20 15:42:21 +0100

    feat(FRONT): connect socket only if not connected

--- Client/src/App.tsx
+++ Client/src/App.tsx
@@ -2,7 +2,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Provider } from "react-redux";
 import { routes, store, useAppDispatch, useAppSelector } from "./core";
 import RequireAuth from "./core/RequireAuth";
-import React, { ReactNode } from "react";
+import React, { ReactNode, useEffect, useState } from "react";
 import { useAuthentication } from "./packages/feat-Auth/authUtils";
 import { ConnectSocket, disconnectSocket } from "./packages";
 
@@ -12,14 +12,18 @@ interface RequireAuthProps {
 
 const SocketInit = (props: any) => {
   console.log("SocketInit");
+  const socket = useAppSelector((state) => state.socket);
   const isAuthenticated = useAuthentication();
   const dispatch = useAppDispatch();
-  if (isAuthenticated) {
+
+  if (isAuthenticated && !socket.isConnected) {
     console.log("connectSocket");
     dispatch(ConnectSocket());
   }
+
   return props.children;
 };
+
 
 function App() {
   console.log("app Rendering");
