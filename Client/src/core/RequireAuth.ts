8f118e76cd6662c55b1ae477cc1ba6fd72bd92ee
sdk-meb <you@sdk.com>
2023-11-17 09:16:31 +0100

    feat(FRONT): navigation to overview in place of home, in case of no login.

--- Client/src/core/RequireAuth.ts
+++ Client/src/core/RequireAuth.ts
@@ -4,33 +4,33 @@ import { useAuthentication } from "../packages/feat-Auth/authUtils";
 import { useAppSelector } from "./redux";
 
 interface RequireAuthProps {
-    children: React.ReactElement;
+  children: any;
 }
 
 const RequireAuth: React.FC<RequireAuthProps> = ({ children }) => {
-    const isAuthenticated = useAuthentication();
-    // console.log(
-    //   "RequireAuth Rendering",
-    //   children.props.children.type.name,
-    //   isAuthenticated
-    // );
-    const navigate = useNavigate();
-    const authState = useAppSelector((state) => state.auth);
-    const isFirstLogin = authState.firstLogin;
-    React.useEffect(() => {
-        if (!isAuthenticated) {
-            navigate("/login");
-        }
-        if (isFirstLogin) {
-            navigate("/firstlogin");
-        }
-        if (authState.shouldVerifyTwoFactor) {
-            navigate("/verifyOtp");
-        }
-    }, [isAuthenticated]);
+  const isAuthenticated = useAuthentication();
+  // console.log(
+  //   "RequireAuth Rendering",
+  //   children.props.children.type.name,
+  //   isAuthenticated
+  // );
+  const navigate = useNavigate();
+  const authState = useAppSelector((state) => state.auth);
+  const isFirstLogin = authState.firstLogin;
+  React.useEffect(() => {
+    if (!isAuthenticated) {
+      children.path === "/" ? navigate("/overView") : navigate("/login");
+    }
+    if (isFirstLogin) {
+      navigate("/firstlogin");
+    }
+    if (authState.shouldVerifyTwoFactor) {
+      navigate("/verifyOtp");
+    }
+  }, [isAuthenticated]);
 
-    // Render the wrapped content if authenticated
-    return children;
+  // Render the wrapped content if authenticated
+  return children.element;
 };
 
 export default RequireAuth;
