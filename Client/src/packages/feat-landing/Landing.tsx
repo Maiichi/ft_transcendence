f7d3a35877c77e75cba3769c1a76cb4112975a6f
sdk-meb <you@sdk.com>
2023-11-25 23:13:31 +0100

    feat(FRONT):  landing page on login Click => Backdrop blackblery

--- Client/src/packages/feat-landing/Landing.tsx
+++ Client/src/packages/feat-landing/Landing.tsx
@@ -63,9 +63,7 @@ const LandingPage = () => {
               sx={{ color: "#fff", zIndex: (theme) => theme.zIndex.drawer + 1 }}
               open={open}
               onClick={handleClose}
-            >
-              {/* <CircularProgress color="inherit" /> */}
-            </Backdrop>
+            ></Backdrop>
           </div>
         </div>
       </header>
