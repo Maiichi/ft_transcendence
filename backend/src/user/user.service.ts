e8e2305d1aef9189bb6a2e95caf79db2adf9333b
ishakzail <ishak.zail.iz@gmail.com>
2023-12-23 18:16:15 +0100

    feat(BACK): get Friend Requests sent to the connectedUser

--- backend/src/user/user.service.ts
+++ backend/src/user/user.service.ts
@@ -423,5 +423,21 @@ export class UserService {
         
         return res.send({data : users});
     }
+
+    // get friendsRequests sent the to the current user
+    async getFriendRequests(userId: number, res: Response)
+    {
+        const friendRequests = await this.prisma.friendRequest.findMany({
+            where : {
+                receiverId : userId,
+            },
+            select : {
+                sender: true,
+            }
+        });
+        let requests = [];
+        friendRequests.forEach((request) => requests.push(request.sender));
+        return res.send(requests);
+    }
     
 }
